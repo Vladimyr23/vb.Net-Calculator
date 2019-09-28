8736a0f02df7a1fc4aa13e202e1c2122ed4056aa
Vladimyr23 <33516849+Vladimyr23@users.noreply.github.com>
2019-09-28 14:55:33 +0100

    Add files via upload

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.txtAnswer = New System.Windows.Forms.TextBox()
+        Me.btnpoint = New System.Windows.Forms.Button()
+        Me.btnequals = New System.Windows.Forms.Button()
+        Me.btnplus = New System.Windows.Forms.Button()
+        Me.btnsubstract = New System.Windows.Forms.Button()
+        Me.btnmultyply = New System.Windows.Forms.Button()
+        Me.btndivide = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(12, 198)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(41, 41)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(60, 198)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(41, 41)
+        Me.btn2.TabIndex = 1
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(106, 198)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(41, 41)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(12, 151)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(41, 41)
+        Me.btn4.TabIndex = 3
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(59, 151)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(41, 41)
+        Me.btn5.TabIndex = 4
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(106, 151)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(41, 41)
+        Me.btn6.TabIndex = 5
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(13, 104)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(41, 41)
+        Me.btn7.TabIndex = 6
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(59, 104)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(41, 41)
+        Me.btn8.TabIndex = 7
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(106, 104)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(41, 41)
+        Me.btn9.TabIndex = 8
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn0.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.Location = New System.Drawing.Point(60, 245)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(41, 41)
+        Me.btn0.TabIndex = 9
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'txtAnswer
+        '
+        Me.txtAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAnswer.Location = New System.Drawing.Point(13, 26)
+        Me.txtAnswer.MaxLength = 10
+        Me.txtAnswer.Name = "txtAnswer"
+        Me.txtAnswer.Size = New System.Drawing.Size(256, 49)
+        Me.txtAnswer.TabIndex = 10
+        Me.txtAnswer.Text = "0"
+        Me.txtAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnpoint
+        '
+        Me.btnpoint.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnpoint.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnpoint.Location = New System.Drawing.Point(13, 245)
+        Me.btnpoint.Name = "btnpoint"
+        Me.btnpoint.Size = New System.Drawing.Size(41, 41)
+        Me.btnpoint.TabIndex = 11
+        Me.btnpoint.Text = "."
+        Me.btnpoint.UseVisualStyleBackColor = False
+        '
+        'btnequals
+        '
+        Me.btnequals.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnequals.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnequals.Location = New System.Drawing.Point(106, 245)
+        Me.btnequals.Name = "btnequals"
+        Me.btnequals.Size = New System.Drawing.Size(107, 41)
+        Me.btnequals.TabIndex = 12
+        Me.btnequals.Text = "="
+        Me.btnequals.UseVisualStyleBackColor = False
+        '
+        'btnplus
+        '
+        Me.btnplus.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnplus.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnplus.Location = New System.Drawing.Point(153, 104)
+        Me.btnplus.Name = "btnplus"
+        Me.btnplus.Size = New System.Drawing.Size(60, 88)
+        Me.btnplus.TabIndex = 13
+        Me.btnplus.Text = "+"
+        Me.btnplus.UseVisualStyleBackColor = False
+        '
+        'btnsubstract
+        '
+        Me.btnsubstract.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnsubstract.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsubstract.Location = New System.Drawing.Point(153, 198)
+        Me.btnsubstract.Name = "btnsubstract"
+        Me.btnsubstract.Size = New System.Drawing.Size(60, 41)
+        Me.btnsubstract.TabIndex = 14
+        Me.btnsubstract.Text = "-"
+        Me.btnsubstract.UseVisualStyleBackColor = False
+        '
+        'btnmultyply
+        '
+        Me.btnmultyply.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnmultyply.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmultyply.Location = New System.Drawing.Point(219, 104)
+        Me.btnmultyply.Name = "btnmultyply"
+        Me.btnmultyply.Size = New System.Drawing.Size(50, 55)
+        Me.btnmultyply.TabIndex = 15
+        Me.btnmultyply.Text = "*"
+        Me.btnmultyply.UseVisualStyleBackColor = False
+        '
+        'btndivide
+        '
+        Me.btndivide.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btndivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndivide.Location = New System.Drawing.Point(219, 165)
+        Me.btndivide.Name = "btndivide"
+        Me.btndivide.Size = New System.Drawing.Size(50, 52)
+        Me.btndivide.TabIndex = 16
+        Me.btndivide.Text = "/"
+        Me.btndivide.UseVisualStyleBackColor = False
+        '
+        'btnclear
+        '
+        Me.btnclear.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Location = New System.Drawing.Point(219, 225)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(50, 61)
+        Me.btnclear.TabIndex = 17
+        Me.btnclear.Text = "C"
+        Me.btnclear.UseVisualStyleBackColor = False
+        '
+        'frmCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(277, 299)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btndivide)
+        Me.Controls.Add(Me.btnmultyply)
+        Me.Controls.Add(Me.btnsubstract)
+        Me.Controls.Add(Me.btnplus)
+        Me.Controls.Add(Me.btnequals)
+        Me.Controls.Add(Me.btnpoint)
+        Me.Controls.Add(Me.txtAnswer)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.MaximizeBox = False
+        Me.Name = "frmCalculator"
+        Me.ShowIcon = False
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents txtAnswer As TextBox
+    Friend WithEvents btnpoint As Button
+    Friend WithEvents btnequals As Button
+    Friend WithEvents btnplus As Button
+    Friend WithEvents btnsubstract As Button
+    Friend WithEvents btnmultyply As Button
+    Friend WithEvents btndivide As Button
+    Friend WithEvents btnclear As Button
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,276 @@
+﻿Public Class frmCalculator
+
+    ''Variables declaration
+
+    Dim marker As Integer
+    Dim total As Decimal
+    Dim pend As Decimal
+    Dim job As String
+    Dim operation As String
+
+    ''Button '.'(dot)
+
+    Private Sub btnpoint_Click(sender As Object, e As EventArgs) Handles btnpoint.Click
+        If Not txtAnswer.Text.Contains(".") And marker <> 1 Then
+            txtAnswer.Text &= "."
+        End If
+    End Sub
+
+    ''Button '0'
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "0"
+            marker = 0
+        Else
+            txtAnswer.Text &= "0"
+        End If
+    End Sub
+
+    ''Button '9'
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "9"
+            marker = 0
+        Else
+            txtAnswer.Text &= "9"
+        End If
+    End Sub
+
+    ''Button '8'
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "8"
+            marker = 0
+        Else
+            txtAnswer.Text &= "8"
+        End If
+    End Sub
+
+    ''Button '7'
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "7"
+            marker = 0
+        Else
+            txtAnswer.Text &= "7"
+        End If
+    End Sub
+
+    ''Button '6'
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "6"
+            marker = 0
+        Else
+            txtAnswer.Text &= "6"
+        End If
+    End Sub
+
+    ''Button '5'
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "5"
+            marker = 0
+        Else
+            txtAnswer.Text &= "5"
+        End If
+    End Sub
+
+    ''Button '4'
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "4"
+            marker = 0
+        Else
+            txtAnswer.Text &= "4"
+        End If
+    End Sub
+
+    ''Button '3'
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "3"
+            marker = 0
+        Else
+            txtAnswer.Text &= "3"
+        End If
+    End Sub
+
+    ''Button '2'
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "2"
+            marker = 0
+        Else
+            txtAnswer.Text &= "2"
+        End If
+    End Sub
+
+    ''Button '1'
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If txtAnswer.Text = "0" Or marker = 1 Then
+            txtAnswer.Text = "1"
+            marker = 0
+        Else
+            txtAnswer.Text &= "1"
+        End If
+    End Sub
+
+    ''Button '+'
+
+    Private Sub btnplus_Click(sender As Object, e As EventArgs) Handles btnplus.Click
+
+        If operation = "" Or marker = 1 Then
+            total = CDec(txtAnswer.Text)
+            operation = "+"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "+" Then
+            total = CDec(total) + CDec(txtAnswer.Text)
+            operation = "+"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "-" Then
+            total = CDec(total) - CDec(txtAnswer.Text)
+            operation = "+"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "*" Then
+            total = CDec(total) * CDec(txtAnswer.Text)
+            operation = "+"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "/" Then
+            total = CDec(total) / CDec(txtAnswer.Text)
+            operation = "+"
+            txtAnswer.Text = Str(total)
+        End If
+        marker = 1
+    End Sub
+
+    ''Button '-'
+
+    Private Sub btnsubstract_Click(sender As Object, e As EventArgs) Handles btnsubstract.Click
+
+        If operation = "" Or marker = 1 Then
+            total = CDec(txtAnswer.Text)
+            operation = "-"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "+" Then
+            total = CDec(total) + CDec(txtAnswer.Text)
+            operation = "-"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "-" Then
+            total = CDec(total) - CDec(txtAnswer.Text)
+            operation = "-"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "*" Then
+            total = CDec(total) * CDec(txtAnswer.Text)
+            operation = "-"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "/" Then
+            total = CDec(total) / CDec(txtAnswer.Text)
+            operation = "-"
+            txtAnswer.Text = Str(total)
+        End If
+        marker = 1
+    End Sub
+
+    ''Button '*'
+
+    Private Sub btnmultyply_Click(sender As Object, e As EventArgs) Handles btnmultyply.Click
+
+        If operation = "" Or marker = 1 Then
+            total = CDec(txtAnswer.Text)
+            operation = "*"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "+" Then
+            total = CDec(total) + CDec(txtAnswer.Text)
+            operation = "*"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "-" Then
+            total = CDec(total) - CDec(txtAnswer.Text)
+            operation = "*"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "*" Then
+            total = CDec(total) * CDec(txtAnswer.Text)
+            operation = "*"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "/" Then
+            total = CDec(total) / CDec(txtAnswer.Text)
+            operation = "*"
+            txtAnswer.Text = Str(total)
+        End If
+        marker = 1
+    End Sub
+
+    ''Button '/'
+
+    Private Sub btndivide_Click(sender As Object, e As EventArgs) Handles btndivide.Click
+
+        If operation = "" Or marker = 1 Then
+            total = CDec(txtAnswer.Text)
+            operation = "/"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "+" Then
+            total = CDec(total) + CDec(txtAnswer.Text)
+            operation = "/"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "-" Then
+            total = CDec(total) - CDec(txtAnswer.Text)
+            operation = "/"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "*" Then
+            total = CDec(total) * CDec(txtAnswer.Text)
+            operation = "/"
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "/" Then
+            total = CDec(total) / CDec(txtAnswer.Text)
+            operation = "/"
+            txtAnswer.Text = Str(total)
+        End If
+        marker = 1
+    End Sub
+
+    ''Button '='
+
+    Private Sub btnequals_Click(sender As Object, e As EventArgs) Handles btnequals.Click
+
+        If operation = "" Or marker = 1 Then
+            total = CDec(txtAnswer.Text)
+            operation = ""
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "+" Then
+            total = CDec(total) + CDec(txtAnswer.Text)
+            operation = ""
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "-" Then
+            total = CDec(total) - CDec(txtAnswer.Text)
+            operation = ""
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "*" Then
+            total = CDec(total) * CDec(txtAnswer.Text)
+            operation = ""
+            txtAnswer.Text = Str(total)
+        ElseIf operation = "/" Then
+            total = CDec(total) / CDec(txtAnswer.Text)
+            operation = ""
+            txtAnswer.Text = Str(total)
+        End If
+        marker = 1
+    End Sub
+
+    ''Button 'C'
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        txtAnswer.Text = "0"
+    End Sub
+
+End Class
+
